0ffc113955b40ee2b02bbedde39ef802cc7da065
Nick Douglas <nick.douglas82@gmail.com>
2020-10-02 11:24:51 +0100

    Component builds

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { JumbotronContainer } from './containers/jumbotron';
+import { FaqsContainer } from './containers/faqs';
+import { FooterContainer } from './containers/footer';
 
-export const App = () => <JumbotronContainer />;
+export const App = () => (
+  <>
+    <JumbotronContainer />
+    <FaqsContainer />
+    <FooterContainer />
+  </>
+);
 
 export default App;
